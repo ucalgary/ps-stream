6493db64b87a2e43959590d70db4992e87f72593
King Chung Huang <kinghuang@mac.com>
2017-01-30 14:21:01 -0700

    Create a CLI commands class
    Create a class named PSSyncCommand that will handle command-line
    commands.

--- pssync/collector.py
+++ pssync/collector.py
@@ -1,5 +1,51 @@
+import logging
 from twisted.internet import reactor, endpoints
 from twisted.web import resource, server
+
+
+
+log = logging.getLogger(__name__)
+
+
+class PSSyncCommand(object):
+	"""Collect PeopleSoft sync messages into Kafka topics.
+
+	Usage:
+	  ps-sync-collector [options] [COMMAND] [ARGS...]
+	  ps-sync-collector -h|--help
+
+	Options:
+	  -b, --broker BROKER_HOSTS     Comma-separated list of Kafka broker hosts (default: kafka:9092)
+	  -r, --registry REGISTRY_HOST  URL of the service where Avro schemas are registered
+	  -p, --topic-prefix PREFIX     String to prepend to all topic names
+
+	Commands:
+	  collect            Collect PeopleSoft sync messages
+	  config             Validate and view the collector config
+	"""
+
+	def collect(self, options):
+		"""
+		Collect PeopleSoft sync and fullsync messages.
+
+		Usage: collect
+		"""
+		log.info('collect')
+		log.info(options)
+
+		site = server.Site(PSSyncCollector())
+		endpoint = endpoints.TCP4ServerEndpoint(reactor, 8080)
+		endpoint.listen(site)
+		reactor.run()
+
+	def config(self, options):
+		"""
+		Validate and view the collector config
+
+		Usage: config
+		"""
+		log.info('config')
+		log.info(options)
 
 
 class PSSyncCollector(resource.Resource):
