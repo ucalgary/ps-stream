d55be1300e3c947f66da07e14d0ad339f0a6f7fa
King Chung Huang <kinghuang@mac.com>
2017-01-30 11:32:44 -0700

    Add a stub method for handling POST

--- pssync/receiver.py
+++ pssync/receiver.py
@@ -6,4 +6,7 @@ class PSSyncReceiver(resource.Resource):
 	isLeaf = True
 
 	def render_GET(self, request):
-		return '{"status":"ok"}'.encode('utf-8')
+		return '{"status":"GET ok"}'.encode('utf-8')
+
+	def render_POST(self, request):
+		return '{"status":"POST ok"}'.encode('utf-8')
