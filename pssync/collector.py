17d6eac0181264343056d9cf813f60f9f0a56fff
King Chung Huang <kinghuang@mac.com>
2017-02-13 12:32:36 -0700

    Start implementation of PeopleSoft message handling
    When PeopleSoft POSTs a rowset-based message to the PSSyncCollector
    resource, being handling it by parsing the XML for the PeopleSoft
    message name and the field types.
    
    The field_type function is passsed to element_to_obj() so that the type
    attribute is used as the value for the resulting child elements. This
    produces key-value pairs of fieldtype name and fieldtype type for later
    use.

--- pssync/collector.py
+++ pssync/collector.py
@@ -1,6 +1,11 @@
+import json
+
+from xml.etree import ElementTree
+
 from twisted.internet import endpoints, reactor
 from twisted.web import resource, server
 
+from .utils import element_to_obj
 
 class PSSyncCollector(resource.Resource):
 
@@ -10,8 +15,25 @@ class PSSyncCollector(resource.Resource):
         return '{"status":"GET ok"}'.encode('utf-8')
 
     def render_POST(self, request):
-        print(request.getAllHeaders())
-        print(request.content.read())
+        """Decode PeopleSoft rowset-based messages into transactions, and produce Kafka
+        messages for each transaction. PeopleSoft is expected to POST messages as events
+        occur via SYNC and FULLSYNC services.
+
+        The following URL describes the PeopleSoft Rowset-Based Message Format.
+        http://docs.oracle.com/cd/E66686_01/pt855pbr1/eng/pt/tibr/concept_PeopleSoftRowset-BasedMessageFormat-0764fb.html
+        """
+        psft_message_name = None
+        field_types = None
+
+        # Parse the root element for the PeopleSoft message name and FieldTypes
+        request.content.seek(0,0)
+        for event, e in ElementTree.iterparse(request.content, events=('start', 'end')):
+            if event == 'start' and psft_message_name is None:
+                psft_message_name = e.tag
+            elif event == 'end' and e.tag == 'FieldTypes':
+                field_types = element_to_obj(e, value_f=field_type)
+                break
+
         return '{"status":"POST ok"}'.encode('utf-8')
 
 
@@ -22,3 +44,8 @@ def collect(producer, topic=None, port=8000, senders=None, recipients=None, mess
     endpoint.listen(site)
     print(f'Listening for connections on port {port}')
     reactor.run()
+
+
+def field_type(element):
+    assert('type' in element.attrib)
+    return element.attrib.get('type')
