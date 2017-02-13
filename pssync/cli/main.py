9ababe827eacb660c176e23fc049f843f265695f
King Chung Huang <kinghuang@mac.com>
2017-02-13 15:46:00 -0700

    Remove --zookeeper option
    It turns out confluent-kafka-python does not have a zookeeper option.

--- pssync/cli/main.py
+++ pssync/cli/main.py
@@ -58,7 +58,6 @@ class PSSyncCommand(object):
     Options:
       -k, --kafka HOSTS             Kafka brokers [default: kafka:9092]
       -r, --schema-registry URL     Avro schema registry host [default: http://schema-registry:80]
-      -z, --zookeeper HOST          Zookeeper host [default: zookeeper:2181]
       -p, --topic-prefix PREFIX     String to prepend to all topic names
 
     Commands:
