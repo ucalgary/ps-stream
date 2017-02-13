126e95c1f4842b7ab1f9c12ae5938d15727c15ff
King Chung Huang <kinghuang@mac.com>
2017-02-13 10:23:13 -0700

    Add utilities for converting XML to Python objects
    Add a simplistic function named element_to_obj() for converting XML
    elements into Python objects. This function does not comprehensively
    convert arbitrary XML trees. It creates simple trees of dictionaries
    for XML elements, and extracts the node text for end child nodes. The
    value_f argument can be set to customize the value for end child nodes.

---
+++ pssync/utils.py
@@ -0,0 +1,19 @@
+def element_text(element):
+    value = element.text
+    if value:
+        value.strip()
+    return value
+
+
+def element_to_obj(element, map_class=dict, value_f=element_text, wrap_value=True):
+    value = None
+    
+    if len(element) > 0:
+        child_values = map(lambda e: (e.tag, element_to_obj(e, wrap_value=False)), element)
+        value = map_class(child_values)
+    else:
+        value = value_f(element)
+
+    if wrap_value:
+        value = {element.tag: value}
+    return value
