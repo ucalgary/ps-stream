c3cf812b12c6bebdfba5d4c76848bde463819070
King Chung Huang <kinghuang@mac.com>
2017-02-13 10:28:08 -0700

    Pass arguments to inner invocations of element_to_obj
    Pass all the passed-in options for element_to_obj() to inner
    invocations when processing child elements. Otherwise, the options set
    by the original caller are not maintained.

--- pssync/utils.py
+++ pssync/utils.py
@@ -9,7 +9,7 @@ def element_to_obj(element, map_class=dict, value_f=element_text, wrap_value=Tru
     value = None
     
     if len(element) > 0:
-        child_values = map(lambda e: (e.tag, element_to_obj(e, wrap_value=False)), element)
+        child_values = map(lambda e: (e.tag, element_to_obj(e, map_class=map_class ,value_f=value_f, wrap_value=False)), element)
         value = map_class(child_values)
     else:
         value = value_f(element)
