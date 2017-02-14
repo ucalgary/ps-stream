d3ce3f6ce732db97bb303aaa9e3c0dbb7fe11580
King Chung Huang <kinghuang@mac.com>
2017-02-14 15:58:12 -0700

    Create a shell class for PSSyncPublisher
    The PSSyncPublisher class contains the main implementation that listens
    for transaction messages and processes them into record streams.

--- pssync/publisher.py
+++ pssync/publisher.py
@@ -1,2 +1,18 @@
-def publish(consumer, producer, source_topic=None, destination_topic=None):
-    pass
+class PSSyncPublisher(object):
+
+    def __init__(self, consumer, producer, source_topics=None, destination_topic=None):
+        super().__init__()
+        self.consumer = consumer
+        self.producer = producer
+        self.source_topics = source_topics
+        self.destination_topic = destination_topic
+        self.running = True
+
+    def run(self):
+        pass
+
+
+def publish(consumer, producer, source_topics=None, destination_topic=None):
+    publisher = PSSyncPublisher(consumer, producer, source_topics=None, destination_topic=None)
+    print(f'Reading transactions from {source_topics}')
+    publisher.run()
