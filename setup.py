ec86379d4b48870157b71806ddb061824cf37bbb
King Chung Huang <kinghuang@mac.com>
2017-03-15 12:09:28 -0600

    Rename Python module to ps_stream
    Rename the Python module to ps_stream, to match the final naming scheme
    in related projects.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 
 
 setup(
-    name='pssync',
+    name='ps_stream',
     description='Process PeopleSoft sync messages into Kafka topics',
     author='King Chung Huang',
     packages=find_packages(),
@@ -22,7 +22,7 @@ setup(
     install_requires=install_requires,
     entry_points="""
     [console_scripts]
-    pssync=pssync.cli.main:main
+    pssync=ps_stream.cli.main:main
     """,
     zip_safe=True
 )
